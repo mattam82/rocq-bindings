4089f6c485fc3b088199339020b4fe9013af43cc
Matthieu Sozeau <matthieu.sozeau@inria.fr>
2025-01-16 14:32:58 +0100

    Initial release

---
+++ README.md
@@ -0,0 +1,21 @@
+# rocq-bindings README
+
+Expose some useful commands for developing on Rocq and with Rocq.
+
+## Features
+
+- toggleNoInit (toggles the '-noinit' flag in your coqtop.args workspace variable)
+- 
+## Requirements
+
+Requires the VSCoq legacy (1.*) plugin to be useful.
+
+## Known Issues
+
+N/A
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of rocq-bindings
---
+++ src/extension.ts
@@ -0,0 +1,33 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('The "rocq-bindings" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('rocq-bindings.toggleNoInit', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		let config = vscode.workspace.getConfiguration('coqtop');
+		let args = config.get<string[]>('args') ?? [];
+		if (args.includes('-noinit') === true)
+		{	args.filter((arg) => arg !== '-noinit');	}
+		{ args.push('-noinit'); }
+		config.update ('args', args, vscode.ConfigurationTarget.Workspace);
+
+			vscode.window.showInformationMessage('Changed args to ' + args);
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
